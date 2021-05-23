7cf52f62b834a2a265d30eba68ce0732aca0f6bf
Andrei Shishkin <shishkinam82@gmail.com>
2021-05-24 00:59:57 +0400

    autotest

---
+++ src/test/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 1000_60;
+    boolean registered = true;
+    long expected = 30;
+
+    long actual = service.calculate(amount, registered);
+
+    // производим проверку (сравниваем ожидаемый и фактический):
+    // если true - то PASS
+    // если false - то FAIL
+    boolean passed = expected == actual;
+
+    // выводим результат
+    System.out.println(passed);
+  }
+}
